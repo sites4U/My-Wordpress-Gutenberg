53c932fa54a77db9328310d9e30fa6b395c790f7
Alexei <mostovoi.alexei@gmail.com>
2023-12-08 23:32:52 -0500

    Add CSS for edit

--- src/save.js
+++ src/save.js
@@ -3,9 +3,11 @@ import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 export default function Save( { attributes } ) {
 	const { columns } = attributes;
 	return (
-		<div { ...useBlockProps.save({
-			className: `the-${ columns }-columns`,
-		}) }>
+		<div
+			{ ...useBlockProps.save( {
+				className: `the-${ columns }-columns`,
+			} ) }
+		>
 			<InnerBlocks.Content />
 		</div>
 	);
