bf4eebcdd17d39045232e3294105906034f37300
Alexei <mostovoi.alexei@gmail.com>
2023-12-18 19:56:51 -0500

    Categories

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-date', 'wp-element'), 'version' => '575fbdb0b2805ec01166');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element'), 'version' => '30aefaab29b9945fc638');
--- dynamicblock.php
+++ dynamicblock.php
@@ -8,32 +8,33 @@
  */
 
  function webart_latest_posts_block( $attributes){
-	$arg = array (
-		'post_per_page' => $attributes['postPerPage'],
-		'post_status' => 'publish'
-	);	
-
+	$arg = array(
+		'posts_per_page' => $attributes['postsPerPage'],
+		'post_status' => 'publish',
+		'order' =>  $attributes['order'],
+		'orderby' =>  $attributes['orderBy'],
+	);
+	if(isset($attributes['category'])){
+		$arg['category__in'] = $attributes['category'];
+	}
 	$latest_posts = get_posts($arg);
 
-	$html_text = "<div ".get_block_wrapper_attributes().">";
-
+	$html = "<div ".get_block_wrapper_attributes().">";
 	if(!empty($latest_posts)){
-
 		foreach($latest_posts as $post){
-			$html_text .= "<div>";
-					if($attributes['showImage'] && has_post_thumbnail($post)){
-						$html_text .= get_the_post_thumbnail($post, 'large');
-					}
-					$html_text .= "<time datetime='".esc_attr(get_the_date('c',$post))."'>" .esc_html(get_the_date('', $post)). "</time>";
-					$html_text .= "<h2><a href ='".get_the_permalink($post)."'>".esc_html(get_the_title($post))."</a></h2>";
-			$html_text .="</div>";
+			$html .= "<div>";
+				if($attributes['showImage'] && has_post_thumbnail($post)){
+					$html .= wp_kses_post(get_the_post_thumbnail($post, 'large'));
+				}
+				$html .= "<time datetime='".esc_attr(get_the_date('c', $post))."'>".esc_html(get_the_date('', $post))."</time>";
+				$html .= "<h2><a href='".esc_url(get_the_permalink($post))."'>".esc_html(get_the_title($post))."</a></h2>";
+			$html .= "</div>";
 		}
 	}
-
-	$html_text .= "</div>";
-	return $html_text;
+	$html .= "</div>";
+	return $html;
  }
-
+ 
  function webart_myDynamicBlock_init(){
     register_block_type_from_metadata( __DIR__, array(
 			'render_callback' => 'webart_latest_posts_block'
